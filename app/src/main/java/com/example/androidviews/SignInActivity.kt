dacea79a2f5a23081ca3615c5398686ba755a68a
Kaan Sarıkaya <45636360+kkaansrky@users.noreply.github.com>
2021-07-03 20:15:45 +0300

    sign in activity

---
+++ app/src/main/java/com/example/androidviews/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        actionBar?.hide()
+    }
+}
